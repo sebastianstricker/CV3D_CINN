5db5410ab34b6c7280f7720fa73eb0cc2c9a29fa
Sebastian Stricker <nw258@uni-heidelberg.de>
2022-03-14 12:41:01 +0100

    finished toy project

--- Toy_project/data.py
+++ Toy_project/data.py
@@ -1,12 +1,19 @@
+from collections import defaultdict
+import numpy as np
+
 import torch
 from torch.utils.data       import Dataset
 from torch.distributions    import MultivariateNormal
 
-distribution_centers = [[3.0,3.0], [-5.0,-5.0]]
+from matplotlib import pyplot as plt
+from matplotlib import patches as plot_patches
+
+markers = ['o', 's', '^'] # circle, square, triangle
+colors = ['red', 'green', 'blue', 'pink', 'black', 'purple']
 
 class GaussianDistributions(Dataset):
 
-    def __init__(self, nr_samples, transform=None):
+    def __init__(self, nr_samples, distribution_centers, style=False):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -15,16 +22,23 @@ class GaussianDistributions(Dataset):
                 on a sample.
         """
         self.nr_samples = nr_samples
-        self.transform = transform
+        self.nr_distributions = len(distribution_centers)
 
         # Create a distribution for each given center point
-        distributions = [MultivariateNormal(torch.tensor(center), torch.eye(2)) for center in distribution_centers]
+        distributions = [MultivariateNormal(torch.tensor(center), torch.eye(len(center))) for center in distribution_centers]
         
         # Create samples and labels for each distribution
         self.data = []
+        shapes = torch.tensor([0,1,2])
+
         for label, distribution in enumerate(distributions):
             for i in range(nr_samples):
-                self.data.append((distribution.sample(), label))
+                sample = distribution.sample()
+
+                if style:
+                    shape = torch.randint(low=0, high=len(shapes), size=(1,))
+                    sample = torch.cat((sample, shape))
+                self.data.append((sample, label))
                 
     def __len__(self):
         return len(self.data)
@@ -35,7 +49,64 @@ class GaussianDistributions(Dataset):
         
         sample = self.data[idx]
 
-        if self.transform:
-            sample = self.transform(sample)
-
         return sample
+
+def plot_distributions(data):
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal', 'box')
+    ax.grid(True)
+
+    # Only plot datapoints
+    if len(data[0]) == 2:
+        x1,y1 = data.T
+        ax.scatter(x1,y1, s=plt.rcParams['lines.markersize'])
+
+    # Plot datapoints, color by labelling
+    elif len(data[0]) == 3:
+        legend_handles = []
+
+        # group data by label
+        label_dict = defaultdict(list)
+        for x, y, label in data:
+            label_dict[label].append([x,y])
+        
+        # plot with color by label
+        for label, datapoints in label_dict.items():
+            legend_handles.append(plot_patches.Patch(color=colors[int(label)], label=f'label {int(label)}'))
+
+            x1,y1 = np.array(datapoints).T
+            ax.scatter(x1,y1, s=plt.rcParams['lines.markersize'], label=f'label {int(label)}', color=colors[int(label)]) 
+       
+        ax.legend(handles=legend_handles)
+
+    # Plot datapoints, color & shape
+    elif len(data[0]) == 4:
+        invalid_shape_count = 0
+        legend_handles = []
+
+        # group data by label
+        label_dict = defaultdict(list)
+        for x, y, shape, label in data:
+            label_dict[label].append([x,y, shape])
+        
+        for label, group in label_dict.items():
+            legend_handles.append(plot_patches.Patch(color=colors[int(label)], label=f'label {int(label)}'))
+
+            # group data by shape
+            shape_dict = defaultdict(list)
+            for x, y, shape in group:
+                s = round(shape)
+                if s < 0 or s > 2:
+                    invalid_shape_count += 1
+                    continue
+                shape_dict[s].append([x,y])
+
+            for shape, datapoints in shape_dict.items():
+                # plot with color by label and shape
+                x1,y1 = np.array(datapoints).T
+                ax.scatter(x1,y1, s=plt.rcParams['lines.markersize'], label=f'label {int(label)}', marker=markers[shape], color=colors[int(label)])
+       
+        ax.legend(handles=legend_handles)
+        print(f"Shape class invalid: {str(invalid_shape_count)} of {len(data)}")
+
+    plt.show()
---
+++ Toy_project/train.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import FrEIA.framework as Ff
+import FrEIA.modules as Fm
+
+import time
+
+def train(inn, train_loader, test_loader, learning_rate, weight_decay, gamma, epochs):
+    inn.cuda()
+
+    trainable_parameters = [p for p in inn.parameters() if p.requires_grad]
+
+    optimizer = torch.optim.Adam(trainable_parameters, lr=learning_rate, weight_decay=weight_decay)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[i for i in range(0,epochs,10)], gamma=gamma)
+
+    val_x, val_l = zip(*list(sample for sample in test_loader.dataset))
+    val_x = torch.stack(val_x, 0).cuda()
+    val_l = torch.LongTensor(val_l).cuda()
+
+    t_start = time.time()
+    nll_list = []
+    print('Epoch\tBatch/Total \tTime \tNLL train\tNLL val\t\tLR')
+    for epoch in range(epochs):
+        for batch, (x, l) in enumerate(train_loader):
+            x, l = x.cuda(), l.cuda()
+
+            z, log_j = inn(x, l)
+
+            nll = torch.mean(z**2) / 2 - torch.mean(log_j) / inn.dims
+            nll.backward()
+            torch.nn.utils.clip_grad_norm_(trainable_parameters, 10.)
+            nll_list.append(nll.item())
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if not batch % 50:
+                curr_lr = optimizer.param_groups[0]['lr']
+                print_training_progress(inn, val_x, val_l, t_start, epoch, batch, len(train_loader), np.mean(nll_list), curr_lr)
+                nll_list = []
+        scheduler.step()
+
+    return inn
+
+def print_training_progress(inn, testdata_x, testdata_l, t_start, epoch, batch, nr_batches, nll_mean, curr_lr):
+    with torch.no_grad():
+        z, log_j = inn(testdata_x, testdata_l)
+        nll_val = torch.mean(z**2) / 2 - torch.mean(log_j) / inn.dims
+
+    print('%.3i \t%.5i/%.5i \t%.2f \t%.6f\t%.6f\t%.2e' % (epoch,
+                                                    batch, nr_batches,
+                                                    (time.time() - t_start)/60.,
+                                                    nll_mean,
+                                                    nll_val.item(),
+                                                    curr_lr,
+                                                    ), flush=True)
